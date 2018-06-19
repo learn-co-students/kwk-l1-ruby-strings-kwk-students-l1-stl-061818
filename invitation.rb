442004f4d0436a9679f2cf3333a541598f64e663
abreternitz <aliceburian@gmail.com>
2018-06-19 14:11:06 +0000

    Done.

--- invitation.rb
+++ invitation.rb
@@ -1,3 +1,19 @@
-# Code your prompts here!
+ # Code your prompts here!
+puts "What's your guest's name?"
+ guest_name=gets.chomp.capitalize
+ puts "What's the name of your party?"
+ party_name=gets.chomp.capitalize
+ puts "When's your party?"
+ date=gets.chomp.capitalize
+ puts "What time is your party?"
+ time=gets.chomp.capitalize
+ puts "When should your guests RSVP?"
+ rsvp=gets
+ puts "what is your name?"
+host_name=gets.chomp.capitalize
 
-# Try starting out with puts'ing a string.
+
+puts "Dear #{guest_name},
+  You are cordially invited to the #{party_name} on #{date} at #{time}. Please RSVP by #{rsvp}
+    Sincerly,
+      #{host_name}"
